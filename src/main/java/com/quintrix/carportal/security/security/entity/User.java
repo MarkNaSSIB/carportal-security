8831091bf4aba9e8ad6970a66d6e1333a5306013
MarkNaSSIB <markblankboutros@gmail.com>
2022-05-30 13:44:35 -0400

    On branch develop
     implemented user entity

--- src/main/java/com/quintrix/carportal/security/security/entity/User.java
+++ src/main/java/com/quintrix/carportal/security/security/entity/User.java
@@ -1,5 +1,61 @@
 package com.quintrix.carportal.security.security.entity;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String fullName;
+  private String password;
+  private String role;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getFullName() {
+    return fullName;
+  }
+
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getRole() {
+    return role;
+  }
+
+  public void setRole(String role) {
+    this.role = role;
+  }
+
+  public User(Long id, String fullName, String password, String role) {
+    super();
+    this.id = id;
+    this.fullName = fullName;
+    this.password = password;
+    this.role = role;
+  }
+
 
 }
